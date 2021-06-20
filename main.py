f0b70d870ff61e4cb684b976b57ca05d57b8b0b5
kaygu <kaygumc@gmail.com>
2021-06-20 19:24:19 +0200

    add Exception handeling, check if files exists, remove files after execution

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-import pandas as pd
-from utils.split_geotiffs import parse_geotiffs, split_tiles
+from utils.handle_tiles import parse_geotiffs, split_tile, get_tile, check_tiff_files, clean_tiles
 
 # {} = 'DTM' or 'DSM'
-INPUT_PATH = './data/{}/'
-OUTPUT_PATH = './data/{}_split/'
+DATA_PATH = './data/{}/'
+TILES_PATH = './data/{}_split/'
 
 
 if __name__ == "__main__":
-    tiles = parse_geotiffs(input_path=INPUT_PATH)
-    split_tiles(tiles.loc[555], input=INPUT_PATH, output=OUTPUT_PATH)
+    # Check if all files needed exist before execution, then store every tiles info in DataFrame
+    check_tiff_files()
+    tiles = parse_geotiffs(path=DATA_PATH)
 
+    # Main loop
+    tile = get_tile(tiles, (218886.35, 220360.26))
+    split_tile(tile, input=DATA_PATH, output=TILES_PATH)
+
+    # Clean tile files after execution
+    clean_tiles(tiles_path= TILES_PATH)
